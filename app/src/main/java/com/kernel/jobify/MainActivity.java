e7b3f9f02b1ea7f893771bace56f032ac838abd2
Mr.BoT <suyog.yadav.60@gmail.com>
2019-10-11 22:36:27 +0530

    firebase update working on services

--- app/src/main/java/com/kernel/jobify/MainActivity.java
+++ app/src/main/java/com/kernel/jobify/MainActivity.java
@@ -6,20 +6,40 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
 
+    FirebaseDatabase database;
+    DatabaseReference databaseRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        database = FirebaseDatabase.getInstance();
+        databaseRef = database.getReference("jobs");
         String[] title = {"News Title Here","News Title Here","News Title Here","News Title Here","News Title Here"};
         String[] disc = {"News Discription Here","News Discription Here","News Discription Here","News Discription Here","News Discription Here",};
+        pushjobs(title,disc);
         Log.i("ABCD","he tr dis bc");
         RecyclerView res  = findViewById(R.id.rscview);
         res.setLayoutManager(new LinearLayoutManager(this));
         res.setAdapter(new NewsAdapter(title,disc));
+
+    }
+
+    public void pushjobs(String[] title,String[] disc)
+    {
+        Log.i("ABCD","push jobs call zhalay");
+        for (int i=0;i<title.length;i++)
+        {
+            String id = databaseRef.push().getKey();
+            Log.i("ABCD",""+id);
+            databaseRef.child("job"+i).setValue(title[i]);
+
+        }
 
     }
 }
